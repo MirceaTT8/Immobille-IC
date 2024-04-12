3b7fc701217652a390ea8d376156a81927764a88
MirceaTT8 <mircea.munteanu356@gmail.com>
2024-04-12 21:50:15 +0300

    solved backend connection problem

---
+++ backend_immobille/server.js
@@ -0,0 +1,44 @@
+const dotenv = require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const userRoute = require("./routes/userRoute");
+const errorHandler = require("./middleware/errorMiddleware");
+
+mongoose.set('strictQuery', true); // Suppress the warning and maintain current behavior
+
+const app = express();
+
+//MiddleWares
+app.use(express.json());
+app.use(cookieParser());
+app.use('/api', userRoute); // mounting the router on the /api path
+app.use(express.urlencoded({extended : false}));
+app.use(cors({
+    origin: 'http://localhost:4200', // Allow only this origin to access
+    credentials: true, // Allow cookies to be sent
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Specify methods to allow
+    allowedHeaders: ['Content-Type', 'Authorization'] // Specify headers to allow
+    }));
+
+//Routes
+app.use("/api/users", userRoute);
+
+app.get("/", (req, res) => {
+    res.send("Home Page...");
+});
+
+
+//Error middleware
+app.use(errorHandler)
+const PORT = process.env.PORT || 5002;
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+        app.listen(PORT, ()=> {
+            console.log(`Server running on port ${PORT}`);
+        });
+    })
+    .catch((err) => console.log(err))
+
