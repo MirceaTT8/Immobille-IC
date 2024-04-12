67164349312b13f457e21737d46314b653a62303
MirceaTT8 <mircea.munteanu356@gmail.com>
2024-04-12 23:39:31 +0300

    implemented data binding

--- Immobille/src/app/listing/listing.component.ts
+++ Immobille/src/app/listing/listing.component.ts
@@ -1,84 +1,99 @@
-import { Component, Input } from '@angular/core';
-import { CommonModule } from '@angular/common';
+// @ts-ignore
+
+import { Component } from '@angular/core';
 import { Advertisement } from '../advertisement';
+import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-listing',
   standalone: true,
-  imports: [],
+  imports: [
+    NgOptimizedImage
+  ],
   template:`
-  <div id="main">
-           <form id="filter-form">
-               <label for="price-min">Minimum Price:</label>
-               <input type="number" id="price-min" name="price-min" placeholder="Min Price" value="0">
-               <label for="price-max">Maximum Price:</label>
-               <input type="number" id="price-max" name="price-max" placeholder="Max Price" value="100000000">
+    <div id="main">
+      <form id="filter-form">
+        <label for="price-min">Minimum Price:</label>
+        <input type="number" id="price-min" name="price-min" placeholder="Min Price" value="0">
+        <label for="price-max">Maximum Price:</label>
+        <input type="number" id="price-max" name="price-max" placeholder="Max Price" value="100000000">
 
-               <label>Property Type:</label>
-               <select id="property-type" name="property-type">
-                   <option value="any">Any</option>
-                   <option value="house">House</option>
-                   <option value="penthouse">Penthouse</option>
-                   <option value="apartment">Apartment</option>
-                   <option value="terrain">Terrain</option>
-               </select>
+        <label>Property Type:</label>
+        <select id="property-type" name="property-type">
+          <option value="any">Any</option>
+          <option value="house">House</option>
+          <option value="penthouse">Penthouse</option>
+          <option value="apartment">Apartment</option>
+          <option value="terrain">Terrain</option>
+        </select>
 
-               <label>Location:</label>
-               <select id="location" name="location">
-                   <option value="any">Any</option>
-                   <option value="cluj">Cluj</option>
-                   <option value="timisoara">Timisoara</option>
-                   <option value="bucharest">Bucharest</option>
-                   <option value="other">Other</option>
-               </select>
-               
-               <label for="for-sale">For Sale</label>
-               <input type="radio" id="for-sale" name="sale-or-rent" value="sale" checked="true">
-               <label for="for-rent">For Rent:</label>
-               <input type="radio" id="for-rent" name="sale-or-rent" value="rent">
-               <button type="submit">Apply Filter</button>
-           </form>
+        <label>Location:</label>
+        <select id="location" name="location">
+          <option value="any">Any</option>
+          <option value="cluj">Cluj</option>
+          <option value="timisoara">Timisoara</option>
+          <option value="bucharest">Bucharest</option>
+          <option value="other">Other</option>
+        </select>
 
-       <ul class="announcement-list" id="ads-list">
-         <li class="announcement">
-           <img src="../../assets/house1.jpg" alt="Property Image">
-           <div class="announcement-details">
-             <p class="status">For Sale</p>
-             <p class="property-type">House</p>
-             <h2>Elegant Family Home</h2>
-             <p>Spacious family home in a prestigious neighborhood. Perfect for growing families looking for a permanent residence close to top schools and amenities.</p>
-             <p class="price">Price: $340,000</p>
-             <p class="location">Los Angeles, CA</p>
-           </div>
-         </li>
-         <li class="announcement">
-           <img src="../../assets/house2.jpg" alt="Property Image">
-           <div class="announcement-details">
-             <p class="status">For Rent</p>
-             <p class="property-type">Apartment</p>
-             <h2>Modern Downtown Apartment</h2>
-             <p>Luxurious and modern apartment with stunning city views, available for rent. Comes fully furnished with all the latest amenities.</p>
-             <p class="price">Price: $1,950/month</p>
-             <p class="location">New York, NY</p>
-           </div>
-         </li>
-         <li class="announcement">
-           <img src="../../assets/house3.jpg" alt="Property Image">
-           <div class="announcement-details">
-             <p class="status">Lease</p>
-             <p class="property-type">Commercial</p>
-             <h2>Office Space Downtown</h2>
-             <p>Prime office space in the heart of the financial district. Ideal for startups and established companies alike.</p>
-             <p class="price">Price: $2,800/month</p>
-             <p class="location">San Francisco, CA</p>
-           </div>
-         </li>
-       </ul>
- </div>
- `,
+        <label for="for-sale">For Sale</label>
+        <input type="radio" id="for-sale" name="sale-or-rent" value="sale" checked="checked">
+        <label for="for-rent">For Rent:</label>
+        <input type="radio" id="for-rent" name="sale-or-rent" value="rent">
+        <button type="submit">Apply Filter</button>
+      </form>
+
+      <ul class="announcement-list" id="ads-list">
+        @for (advertisement of advertisements; track advertisement.description) {
+        <li class="announcement">
+          <img [src]="advertisement.imageUrl" alt="Property Image">
+          <div class="announcement-details">
+            <p class="status">{{ advertisement.status }}</p>
+            <p class="property-type">{{ advertisement.type }}</p>
+            <h2>{{ advertisement.title }}</h2>
+            <p>{{ advertisement.description }}</p>
+            <p class="price">Price: {{ advertisement.price }}</p>
+            <p class="location">{{ advertisement.location }}</p>
+          </div>
+        </li>
+        }
+      </ul>
+    </div>
+  `,
   styleUrl: './listing.component.css'
 })
 export class ListingComponent {
-  @Input() listing!: Advertisement;
+  advertisements: Advertisement[];
 
+  constructor() {
+    this.advertisements = [
+      {
+        status: "For Sale",
+        type: "House",
+        title: "Elegant Family Home",
+        description: "Spacious family home in a prestigious neighborhood. Perfect for growing families looking for a permanent residence close to top schools and amenities.",
+        price: "$340,000",
+        location: "Los Angeles, CA",
+        imageUrl: "../../assets/house1.jpg"
+      },
+      {
+        status: "For Rent",
+        type: "Apartment",
+        title: "Modern Downtown Apartment",
+        description: "Luxurious and modern apartment with stunning city views, available for rent. Comes fully furnished with all the latest amenities.",
+        price: "$1,950/month",
+        location: "New York, NY",
+        imageUrl: "../../assets/house2.jpg"
+      },
+      {
+        status: "Lease",
+        type: "Commercial",
+        title: "Office Space Downtown",
+        description: "Prime office space in the heart of the financial district. Ideal for startups and established companies alike.",
+        price: "$2,800/month",
+        location: "San Francisco, CA",
+        imageUrl: "../../assets/house3.jpg"
+      }
+    ];
+  }
 }
